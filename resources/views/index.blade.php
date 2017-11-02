28e1ca3c599343c072cfb7d921a3d2ceb5e1a0fb
sair <80976512@qq.com>
2017-11-02 10:56:55 +0800

    ignore some

--- resources/views/index.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-@section('content')
-    <el-row class="container">
-        <el-card>
-            welcome
-        </el-card>
-    </el-row>
-@endsection
--- routes/admin.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-Route::view('/', 'admin.index');
-
-Route::get('problems', 'Admin\ProblemsController@problems');
-
-Route::post('addProblem', 'Admin\ProblemsController@addProblem');
